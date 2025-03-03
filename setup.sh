e635d6e0ddeec4742ee5ada0087d2eab1287a99f
asya1098 <arsenijmozgov487@gmail.com>
2025-03-03 12:39:39 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ pkg update && pkg upgrade
 pkg install git
 
 echo "Выберете текстовый редактор для установки"
-echo 
-"1.nano
- 2.vim
- 3.neovim"
+echo -e  " 1.nano\n 2.vim\n 3.neovim\n"
 
+echo "Номер редактора:" 
 read $1
+ 
+if($1)
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "обновление пакетов системы..."
-pkg update && pkg upgrade 
-neofetch
-exit 0
